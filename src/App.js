e89d2302e998cd88921e830dcddfad48fde4541b
garus_ae <garus.ae@weldbook.ru>
2022-10-25 20:13:53 +0300

    counter-finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+  let [count, setCount] = React.useState(1)
+
+  const onClickPlus = () => {
+   setCount(count+ 1)
+  }
+
+  const onClickMinuse = () => {
+    setCount(count-1 )
+   }
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onClickMinuse} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
